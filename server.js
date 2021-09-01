befb901b1daa3c03910c9a044f67d0bf4fed4de2
ferdous85 <ferdous85@gmail.com>
2021-09-01 23:01:55 +0600

    server

---
+++ server.js
@@ -0,0 +1,11 @@
+require('dotenv0').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+

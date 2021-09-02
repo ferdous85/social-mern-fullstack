27a1154716cd2b29f14921eb670c20df94505683
ferdous85 <ferdous85@gmail.com>
2021-09-02 16:46:56 +0600

    frontend added

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv0').config()
+require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
@@ -9,3 +9,20 @@ app.use(express.json())
 app.use(cors())
 app.use(cookieParser())
 
+app.get('/', (req, res)=>{
+  res.json({mes:'Hello'})
+})
+
+//Database
+const URI = process.env.MONGODB_URL
+mongoose.connect(URI, err=>{
+  if(err) throw err;
+  console.log('Database Connected');
+})
+
+
+//Listening PORT
+const port = process.env.PORT || 5000
+app.listen(port, ()=>{
+  console.log('Server is running on port', port);
+})
